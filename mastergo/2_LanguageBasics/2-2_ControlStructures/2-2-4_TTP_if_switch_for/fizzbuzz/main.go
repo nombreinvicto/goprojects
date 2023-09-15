870e0739fea08a69bd3346ba1268a4fcd22c7f61
Mahmud Hasan <mahmud.buet.me@gmail.com>
2023-09-15 05:58:22 -0600

    completed fizzbuzz exercise

--- mastergo/2_LanguageBasics/2-2_ControlStructures/2-2-4_TTP_if_switch_for/fizzbuzz/main.go
+++ mastergo/2_LanguageBasics/2-2_ControlStructures/2-2-4_TTP_if_switch_for/fizzbuzz/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -8,14 +9,29 @@ import (
 func fizzbuzz(n int) {
 
 	// TODO: Implement Fizzbuzz
+	i := 1
+	for i <= n {
+		switch {
+		case i%15 == 0:
+			fmt.Println("FizzBuzz: ", i)
+		case i%5 == 0:
+			fmt.Println("Buzz: ", i)
+		case i%3 == 0:
+			fmt.Println("Fizz: ", i)
+		default:
+			fmt.Println("Nomatch: ", i)
+		}
+		i += 1
+	}
+
 }
 
 func main() {
 	n := 50
 	if len(os.Args) > 1 {
-		max, err := strconv.Atoi(os.Args[1])
+		max_, err := strconv.Atoi(os.Args[1])
 		if err == nil {
-			n = max
+			n = max_
 		}
 	}
 	fizzbuzz(n)
