a91b7b7a7ad016c02621081d7bcc186919550c6e
Mahmud Hasan <mahmud.buet.me@gmail.com>
2023-10-01 23:08:07 -0600

    lenslocked: added contact page handler

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -11,8 +11,20 @@ var portNumber int = 3000
 
 // http.Request is what our browser sends to the server
 // http.ResponseWriter used to write a response to the request
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	// set the response header content type
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err := fmt.Fprint(w, "<h1>Welcome to my awesome site!!!</h1>")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// handler for the contact page
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err := fmt.Fprint(w, "<h1>Contact Page</h1><p>email:<a href=\"mailto:mhasan3.com\"")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -20,7 +32,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// attach handler
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 
 	// start the server
 	serverString := fmt.Sprintf("http://localhost:%d/", portNumber)
