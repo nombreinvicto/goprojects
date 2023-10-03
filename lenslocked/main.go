13a3090ed5db9211e8c5e463b0db18eec323eae5
Mahmud Hasan <mahmud.buet.me@gmail.com>
2023-10-03 01:46:58 -0600

    implemented custom router type implementing ServeHTTP

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -9,13 +9,19 @@ import (
 // declare global vars
 var portNumber int = 3000
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+// listen and serve func requires
+// router that implement ServeHttp method
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// creating a basic router by ourself
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
@@ -42,15 +48,14 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// attach handler
-	http.HandleFunc("/", pathHandler)
-	http.HandleFunc("/contact", pathHandler)
 
 	// start the server
+	var router Router
 	serverString := fmt.Sprintf("http://localhost:%d/", portNumber)
 	fmt.Println(serverString)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", portNumber),
-		nil)
+
+	// pass nil to use default serve mux, mux = router
+	err := http.ListenAndServe(fmt.Sprintf(":%d", portNumber), router)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(-1)
