f1ebe2a3b39344fcaece0f7a2743fa20d47b8005
Mahmud Hasan <mahmud.buet.me@gmail.com>
2023-10-04 05:17:03 -0600

    chap3 branch with go-chi

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 	"os"
 )
@@ -9,26 +10,10 @@ import (
 // declare global vars
 var portNumber int = 3000
 
-// listen and serve func requires
-// router that implement ServeHttp method
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// creating a basic router by ourself
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
-
+// homeHandler is the handler func for homepage
 // http.Request is what our browser sends to the server
 // http.ResponseWriter used to write a response to the request
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-
 	// set the response header content type
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	_, err := fmt.Fprint(w, "<h1>Welcome to my awesome site!!!</h1>")
@@ -49,13 +34,18 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	// start the server
-	var router Router
-	serverString := fmt.Sprintf("http://localhost:%d/", portNumber)
-	fmt.Println(serverString)
+	// instantiate chi router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(func(writer http.ResponseWriter, request *http.Request) {
+		http.Error(writer, "Page Not Found", http.StatusNotFound)
+	})
 
 	// pass nil to use default serve mux, mux = router
-	err := http.ListenAndServe(fmt.Sprintf(":%d", portNumber), router)
+	serverString := fmt.Sprintf("http://localhost:%d/", portNumber)
+	fmt.Println(serverString)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", portNumber), r)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(-1)
