7bad95ffb7656bfc33dfd0fe1c5257b56f5d75f0
Mahmud Hasan <mahmud.buet.me@gmail.com>
2023-10-04 20:01:48 -0600

    lenslocked: rendering templates in handlerfuncs

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"html/template"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 // declare global vars
@@ -14,21 +16,33 @@ var portNumber int = 3000
 // http.Request is what our browser sends to the server
 // http.ResponseWriter used to write a response to the request
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// set the response header content type
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, err := fmt.Fprint(w, "<h1>Welcome to my awesome site!!!</h1>")
-	if err != nil {
-		fmt.Println(err)
-	}
+	templateFilepath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, templateFilepath)
 }
 
 // handler for the contact page
 func contactHandler(w http.ResponseWriter, r *http.Request) {
+	templateFilepath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, templateFilepath)
+}
+
+// common execute template func
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, err := fmt.Fprint(w, "<h1>Contact Page</h1><p>email:"+
-		"<a href=\"mailto:mhasan3.com\"")
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("error parsing template %v", err)
+		http.Error(w, "error parsing template",
+			http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		fmt.Printf("error executing template %v", err)
+		http.Error(w, "error executing template",
+			http.StatusInternalServerError)
+		return
 	}
 }
 
