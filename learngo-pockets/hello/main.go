c753879f1bdab27ccd9e65d4d11a665b12f8433d
Mahmud Hasan <mahmud.buet.me@gmail.com>
2023-10-20 16:23:16 -0600

    initiated pockets project

--- learngo-pockets/hello/main.go
+++ learngo-pockets/hello/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	var lang string
+	flag.StringVar(&lang, "lang",
+		"en", "the required language")
+	flag.Parse()
+
+	greeting := greet(language(lang))
+	fmt.Println(greeting)
+}
+
+// language represents the language's code
+type language string
+
+// phrasebook holds greeting for each supported language
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "םולש םלוע",         // Hebrew
+	"ur": "ﺎﯿﻧد ﻮﻠﯿﮨ",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+	"bn": "শুভেচ্ছা পৃথিবী",   // Bengali
+}
+
+// greet says hello to world in a specific language
+func greet(l language) string {
+	greeting, exists := phrasebook[l]
+	if !exists {
+		return fmt.Sprintf("unsupported language %q", l)
+	}
+	return greeting
 }
--- learngo-pockets/hello/main_internal_test.go
+++ learngo-pockets/hello/main_internal_test.go
@@ -1,7 +1,63 @@
 package main
 
+import "testing"
+
+// testing main() for the right stdout
 func ExampleMain() {
 	main()
 	// Output:
 	// Hello world
+}
+
+func TestGreet(t *testing.T) {
+	type testCase struct {
+		lang language
+		want string
+	}
+
+	var tests = map[string]testCase{
+		"English": {
+			lang: "en",
+			want: "Hello world",
+		},
+		"French": {
+			lang: "fr",
+			want: "Bonjour le monde",
+		},
+		"Akkadian, not supported": {
+			lang: "akk",
+			want: `unsupported language: "akk"`,
+		},
+		"Greek": {
+			lang: "el",
+			want: "Χαίρετε Κόσμε",
+		},
+		"Hebrew": {
+			lang: "he",
+			want: "םולש םלוע",
+		},
+		"Urdu": {
+			lang: "ur",
+			want: "ﺎﯿﻧد ﻮﻠﯿﮨ",
+		},
+		"Vietnamese": {
+			lang: "vi",
+			want: "Xin chào Thế Giới",
+		},
+		"Empty": {
+			lang: "",
+			want: `unsupported language: ""`,
+		},
+	}
+
+	// range over all scenarios
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := greet(tc.lang)
+			if got != tc.want {
+				t.Errorf("expected: %q but got: %q", tc.want, got)
+			}
+		})
+	}
+
 }
