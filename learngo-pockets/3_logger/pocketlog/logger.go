b3937138cfa1b1441dc2859a251720de921774c7
Mahmud Hasan <mahmud.buet.me@gmail.com>
2023-11-04 17:14:27 -0600

    added filelogger options

--- learngo-pockets/3_logger/pocketlog/logger.go
+++ learngo-pockets/3_logger/pocketlog/logger.go
@@ -1,28 +1,62 @@
 package pocketlog
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
 // Debugf formats and prints a message if log level is debug or higher
 func (l *Logger) Debugf(format string, args ...any) {
-	//implement me
+
+	// in case users dont use New but use declaration
+	// to create a logger object with nil values
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+
+	if l.threshold <= LevelDebug {
+		l.logf(format, args...)
+	}
 }
 
 // Infof formats and prints a message if the log level is info or higher
 func (l *Logger) Infof(format string, args ...any) {
-	// implement me
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+
+	if l.threshold <= LevelInfo {
+		l.logf(format, args...)
+	}
 }
 
 // Errorf formats and prints a message if the log level is error or higher
 func (l *Logger) Errorf(format string, args ...any) {
-	// implement me
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+
+	if l.threshold <= LevelError {
+		l.logf(format, args...)
+	}
 }
 
 // /////////////////////////////////////////////////
-// New returns you a logger, ready tolog at required threshold
-func New(threshold Level) *Logger {
+// New returns you a logger, ready to log at required threshold
+// the default output is stdout
+func New(threshold Level, output io.Writer) *Logger {
 	return &Logger{
 		threshold: threshold,
+		output:    output,
 	}
+}
+
+func (l *Logger) logf(format string, args ...any) {
+	fmt.Fprintf(l.output, format+"\n", args...)
 }
